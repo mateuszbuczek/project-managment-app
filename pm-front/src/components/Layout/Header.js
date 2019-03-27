0e034e3b065b57cb9698a0dca7cf46229e072871
unknown <this.mateuszbuczek@gmail.com>
2019-03-27 22:30:34 +0100

    delete operation

--- pm-front/src/components/Layout/Header.js
+++ pm-front/src/components/Layout/Header.js
@@ -6,14 +6,9 @@ class Header extends Component {
       <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
         <div className="container">
           <a className="navbar-brand" href="Dashboard.html">
-            Personal Project Management Tool
+            Project Management App
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-nav"
-          >
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
             <span className="navbar-toggler-icon" />
           </button>
 
