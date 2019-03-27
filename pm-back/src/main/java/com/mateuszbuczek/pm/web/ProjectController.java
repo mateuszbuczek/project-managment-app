4745b318be764d7eb99b9a086d9bbde09fc0df4a
unknown <this.mateuszbuczek@gmail.com>
2019-03-27 07:46:04 +0100

    ValidationMapErrorService

--- pm-back/src/main/java/com/mateuszbuczek/pm/web/ProjectController.java
+++ pm-back/src/main/java/com/mateuszbuczek/pm/web/ProjectController.java
@@ -2,13 +2,18 @@ package com.mateuszbuczek.pm.web;
 
 import com.mateuszbuczek.pm.domain.Project;
 import com.mateuszbuczek.pm.services.ProjectService;
+import com.mateuszbuczek.pm.services.ValidationMapErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.Validation;
 
 @RestController
 @RequestMapping("/api/project")
@@ -16,9 +21,15 @@ public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private ValidationMapErrorService validationMapErrorService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = validationMapErrorService.validationMapErrorService(result);
+        if(errorMap != null) return errorMap;
+
         projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
