6b8a836b8ed5d1ccb6ce5128eff67d4d41a14a52
unknown <this.mateuszbuczek@gmail.com>
2019-03-28 08:16:56 +0100

    backlog and projecttask entities and repos setup

---
+++ pm-back/src/main/java/com/mateuszbuczek/pm/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package com.mateuszbuczek.pm.repositories;
+
+import com.mateuszbuczek.pm.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+}
