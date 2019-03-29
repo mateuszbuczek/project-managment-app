59e22d5233398ec96b602fb2d71873febc267e38
unknown <this.mateuszbuczek@gmail.com>
2019-03-29 08:30:51 +0100

    delete project task

--- pm-front/src/reducers/backlogReducer.js
+++ pm-front/src/reducers/backlogReducer.js
@@ -12,7 +12,13 @@ export default function(state = initialState, action) {
     case GET_PROJECT_TASK:
       return { ...state, project_task: action.payload };
     case DELETE_PROJECT_TASK:
-      return { ...state, project_tasks: state.project_tasks.filter(item => item.id !== action.payload) };
+      return {
+        ...state,
+        project_tasks: state.project_tasks.filter(item => {
+          console.log(item.projectSequence, action.payload);
+          return item.projectSequence !== action.payload;
+        })
+      };
     default:
       return state;
   }
