54bff2f34bed83c24dd12bd83aafa90cf5deedd6
unknown <this.mateuszbuczek@gmail.com>
2019-03-29 20:33:34 +0100

    register and login paths

--- pm-back/src/main/java/com/mateuszbuczek/pm/web/UserContoller.java
+++ pm-back/src/main/java/com/mateuszbuczek/pm/web/UserContoller.java
@@ -1,12 +1,19 @@
 package com.mateuszbuczek.pm.web;
 
 import com.mateuszbuczek.pm.domain.User;
+import com.mateuszbuczek.pm.payload.JWTLoginSuccessResponse;
+import com.mateuszbuczek.pm.payload.LoginRequest;
+import com.mateuszbuczek.pm.security.JwtTokenProvider;
 import com.mateuszbuczek.pm.services.UserService;
 import com.mateuszbuczek.pm.services.ValidationService;
 import com.mateuszbuczek.pm.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import static com.mateuszbuczek.pm.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,11 +37,35 @@ public class UserContoller {
     @Autowired
     UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = validationService.validate(result);
+        if(errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX +  tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
         // validate passwords match and password > 4
         userValidator.validate(user, result);
-
         ResponseEntity<?> errorMap = validationService.validate(result);
         if(errorMap != null) return errorMap;
 
